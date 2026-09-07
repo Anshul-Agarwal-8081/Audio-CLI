d8d7d863edc1f4dc37c9c4fb7c619dd9f8cd9cc0
Anshul-Agarwal-8081 <loveagarwal885@gmail.com>
2026-09-08 00:25:46 +0530

    initialize music player project

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const fs = require("fs");
 
 const files = fs.readdirSync("./songs");
-console.log(files);
+
+const mp3Files = files.filter((file) => file.endsWith(".mp3"));
+
+// Track which song is currently selected
+let selectedIndex = 0;
+
+function displaySongs() {
+    console.clear();
+
+    console.log("🎵 Audio CLI\n");
+
+    mp3Files.forEach((song, index) => {
+        if (index === selectedIndex) {
+            console.log(`> ${index + 1}. ${song}`);
+        } else {
+            console.log(`  ${index + 1}. ${song}`);
+        }
+    });
+}
+
+displaySongs();
